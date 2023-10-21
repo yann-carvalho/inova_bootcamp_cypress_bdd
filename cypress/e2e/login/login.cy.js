80dda5fbd627eff6cdac4aa3a721c9a2913c3faf
Yann Carvalho <yanncarvalho95@gmail.com>
2023-10-20 21:28:13 -0300

    bdd

--- cypress/e2e/login/login.cy.js
+++ cypress/e2e/login/login.cy.js
@@ -5,7 +5,16 @@ context('Acesso a página de login', () =>{
         cy.visit('https://www.saucedemo.com/')
     });
 
+    afterEach(() => {
+        cy.clearAllSessionStorage({log: true})
+        cy.screenshot()
+    });
+
     it('Login válido', () => {
-        
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
+        cy.get('.title').should('contain', 'Products')
+        cy.get('.title').should('have.text', 'Products')
     });
 })
